76643a22958287fdfbc7f59dc62be20ec2c1e0e1
Marco Langebeeke <marcolangebeeke@users.noreply.github.com>
2016-01-19 11:36:49 +0100

    MM1-10: Created custom theme for home- and login screen, with transparent status bar, and modified theme colors to material palette grey/amber

--- app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/MainActivity.java
+++ app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/MainActivity.java
@@ -1,7 +1,7 @@
 package com.amsterdam.marktbureau.makkelijkemarkt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
