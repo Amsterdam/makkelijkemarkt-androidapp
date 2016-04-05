be94c13d4ba23da60f6c4ba13b97175e59da14d3
Marco Langebeeke <marco@langebeeke.com>
2016-04-05 11:02:02 +0200

    Implemented Google Analytics

--- app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/MakkelijkeMarktApplication.java
+++ app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/MakkelijkeMarktApplication.java
@@ -9,39 +9,30 @@ import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 
 /**
- *
+ * Makkelijke Markt Application object
  * @author marcolangebeeke
  */
 public class MakkelijkeMarktApplication extends Application {
 
-    private static GoogleAnalytics mAnalytics;
-
-    private static Tracker mTracker;
-
-    public static GoogleAnalytics analytics() {
-        return mAnalytics;
-    }
-
-    public static Tracker tracker() {
-        return mTracker;
-    }
-
+    /**
+     * Create the Google Analytics tracker
+     */
     @Override
     public void onCreate() {
         super.onCreate();
 
-        mAnalytics = GoogleAnalytics.getInstance(this);
+        GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
 
         // create tracker with our tracker id
-        mTracker = mAnalytics.newTracker("UA-75939009-1");
+        Tracker tracker = analytics.newTracker("UA-75939009-1");
 
         // provide unhandled exceptions reports
-        mTracker.enableExceptionReporting(true);
+        tracker.enableExceptionReporting(true);
 
         // Enable automatic activity tracking for your app
-        mTracker.enableAutoActivityTracking(true);
+        tracker.enableAutoActivityTracking(true);
 
         // Enable automatic activity tracking for your app
-        mTracker.enableAutoActivityTracking(true);
+        tracker.enableAutoActivityTracking(true);
     }
 }
