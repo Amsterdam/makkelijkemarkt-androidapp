f3c497cd828a10e86b68841f65076d9afed952e5
Marco Langebeeke <marco@langebeeke.com>
2016-04-21 17:02:04 +0200

    Removed Geo location logic from DagvergunningActivity

--- app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/DagvergunningActivity.java
+++ app/src/main/java/com/amsterdam/marktbureau/makkelijkemarkt/DagvergunningActivity.java
@@ -3,31 +3,19 @@
  */
 package com.amsterdam.marktbureau.makkelijkemarkt;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 
 import com.amsterdam.marktbureau.makkelijkemarkt.data.MakkelijkeMarktProvider;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
 /**
  *
  * @author marcolangebeeke
  */
 public class DagvergunningActivity extends BaseActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener,
         DagvergunningFragmentKoopman.Callback,
         DagvergunningFragmentProduct.Callback,
         DagvergunningFragmentOverzicht.Callback {
@@ -40,9 +28,6 @@ public class DagvergunningActivity extends BaseActivity implements
 
     // the dagvergunningfragment
     private DagvergunningFragment mDagvergunningFragment;
-
-    // google play services api client
-    private GoogleApiClient mGoogleApiClient;
 
     /**
      * Get markt naam from the shared prefs, set the title and subtitle, and instantiate the
@@ -83,104 +68,12 @@ public class DagvergunningActivity extends BaseActivity implements
             mDrawerFragment.checkItem(mDrawerFragment.DRAWER_POSITION_DAGVERGUNNINGEN);
         }
 
-        // TODO: Implement functionality that disables the location scanning when app in background (to save battery life)
-
-        // build the google play services client
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
         // replace the drawer hamburger with the back-arrow
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
             mDrawerToggle.setDrawerIndicatorEnabled(false);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    /**
-     * Connect to the google play services client
-     */
-    @Override
-    public void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    /**
-     * Disconnect to the google play services client
-     */
-    @Override
-    public void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    /**
-     * When we are connected to the google play services api client start retrieving the locations
-     * and set them in the dagvergunning fragment
-     * @param bundle
-     */
-    @Override
-    public void onConnected(Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            Utility.log(this, LOG_TAG, "Geen toestemming!");
-
-//            // check permissions
-//            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION);
-
-        } else {
-
-//            Utility.log(this, LOG_TAG, "Wel toestemming!");
-
-            // get the last location
-            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            if (lastLocation != null) {
-                mDagvergunningFragment.setRegistratieGeoLocatie(lastLocation);
-            }
-
-            // create a location request that will run on interval every 10 seconds
-            LocationRequest locationRequest = new LocationRequest();
-            locationRequest.setInterval(10000);
-            locationRequest.setFastestInterval(5000);
-            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-            // TODO: implement functionality to request permission
-
-//            // request permission
-//            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-//            PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
-
-            // subscribe to start receiving location updates
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
-        }
-    }
-
-    /**
-     * When we receive a new location from the googleapiclient sent it to the dagvergunning fragment
-     * @param location the received location
-     */
-    @Override
-    public void onLocationChanged(Location location) {
-        Utility.log(this, LOG_TAG, "GoogleApiClient onLocationChanged");
-
-        if (location != null) {
-            mDagvergunningFragment.setRegistratieGeoLocatie(location);
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Utility.log(this, LOG_TAG, "GoogleApiClient onConnectionSuspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Utility.log(this, LOG_TAG, "GoogleApiClient onConnectionFailed");
     }
 
     /**
